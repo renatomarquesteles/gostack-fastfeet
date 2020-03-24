249fb8a2585fd8fd2ba4fec495071084d7fc5590
Renato <renatomarquesteles@gmail.com>
2020-03-24 12:08:35 -0300

    backend: adiciona autenticacao de usuario

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e9a99edfe9476988f95c769a841f7a5e',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Renato' });
-});
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
