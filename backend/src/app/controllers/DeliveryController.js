7f3371e8037db6e99c3bd692e35f3d72a33ea6eb
Renato <renatomarquesteles@gmail.com>
2020-03-28 10:57:25 -0300

    backend: feat: adiciona envio de email

--- backend/src/app/controllers/DeliveryController.js
+++ backend/src/app/controllers/DeliveryController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 
+import Mail from '../../lib/Mail';
 import Delivery from '../models/Delivery';
 import Deliveryman from '../models/Deliveryman';
 import File from '../models/File';
@@ -52,6 +53,27 @@ class DeliveryController {
       recipient_id,
       deliveryman_id,
       product,
+    });
+
+    const deliveryman = await Deliveryman.findByPk(deliveryman_id);
+
+    const recipient = await Recipient.findByPk(recipient_id);
+
+    await Mail.sendMail({
+      to: `${deliveryman.name} <${deliveryman.email}>`,
+      subject: 'Nova encomenda',
+      template: 'newDelivery',
+      context: {
+        deliveryman: deliveryman.name,
+        product,
+        recipient: recipient.name,
+        street: recipient.street,
+        house_number: recipient.house_number,
+        complement: recipient.complement || '-',
+        state: recipient.state,
+        city: recipient.city,
+        zip: recipient.zip,
+      },
     });
 
     return res.json({
