27cff24b1eb6f585a433bf0dda9f5b59ca233cf7
Renato <renatomarquesteles@gmail.com>
2020-03-30 13:20:21 -0300

    frontend: feat: adiciona estilos globais

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 import './config/ReactotronConfig';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
