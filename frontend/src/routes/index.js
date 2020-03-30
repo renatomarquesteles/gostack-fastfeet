96e94992ac574e171a26afdda23ff1d3ee56ca7c
Renato <renatomarquesteles@gmail.com>
2020-03-30 13:06:37 -0300

    frontend: feat: add rotas privadas

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
+import Deliveries from '../pages/Deliveries';
 import Login from '../pages/Login';
+import Route from './Route';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/deliveries" component={Deliveries} isPrivate />
       </Switch>
     </BrowserRouter>
   );
