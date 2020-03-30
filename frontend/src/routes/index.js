3d56f99e6251f643467c2e7c50dc0345c3dcf8ac
Renato <renatomarquesteles@gmail.com>
2020-03-30 13:28:45 -0300

    frontend: chore: add root import

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
 
-import Deliveries from '../pages/Deliveries';
-import Login from '../pages/Login';
+import Deliveries from '~/pages/Deliveries';
+import Login from '~/pages/Login';
 import Route from './Route';
 
 export default function Routes() {
