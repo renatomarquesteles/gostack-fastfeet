688c627363d173ee25efcefc5418cd3e3aa0a008
Renato <renatomarquesteles@gmail.com>
2020-04-01 12:22:27 -0300

    frontend: feat: est inicial tela de encomendas'

--- frontend/src/pages/Deliveries/index.js
+++ frontend/src/pages/Deliveries/index.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import { Content, Tools } from './styles';
+import { DefaultButton } from '~/components/Buttons/styles';
+import SearchInput from '~/components/Form/SearchInput';
 
 export default function Deliveries() {
-  return <div>Entregas</div>;
+  return (
+    <Content>
+      <h1>Gerenciando encomendas</h1>
+      <Tools>
+        <SearchInput placeholder="Buscar por encomendas" />
+        <DefaultButton type="button">
+          <FiPlus size={24} color="#fff" />
+          CADASTRAR
+        </DefaultButton>
+      </Tools>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Destinatário</th>
+          <th>Entregador</th>
+          <th>Cidade</th>
+          <th>Estado</th>
+          <th>Status</th>
+          <th>Ações</th>
+        </tr>
+        <tr>
+          <td>#01</td>
+          <td>Ludwig van Beethoven</td>
+          <td>John Doe</td>
+          <td>Rio do Sul</td>
+          <td>Santa Catarina</td>
+          <td>Entregue</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <td>#01</td>
+          <td>Ludwig van Beethoven</td>
+          <td>John Doe</td>
+          <td>Rio do Sul</td>
+          <td>Santa Catarina</td>
+          <td>Entregue</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <td>#01</td>
+          <td>Ludwig van Beethoven</td>
+          <td>John Doe</td>
+          <td>Rio do Sul</td>
+          <td>Santa Catarina</td>
+          <td>Entregue</td>
+          <td>...</td>
+        </tr>
+      </table>
+    </Content>
+  );
 }
---
+++ frontend/src/pages/Deliveries/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 20px 30px;
+
+  h1 {
+    font-size: 24px;
+    font-weight: bold;
+    color: #444;
+  }
+`;
+
+export const Tools = styled.div`
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
