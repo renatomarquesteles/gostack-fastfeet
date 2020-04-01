91e86c39b589c520385162f7dc0db90fe62209c3
Renato <renatomarquesteles@gmail.com>
2020-04-01 12:21:05 -0300

    frontend: fix: altera header de div para header

--- frontend/src/pages/_layouts/default/styles.js
+++ frontend/src/pages/_layouts/default/styles.js
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   background: #f5f5f5;
 `;
 
-export const Header = styled.div`
+export const Header = styled.header`
   width: 100%;
   height: 64px;
   background: #fff;
