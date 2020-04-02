6895c469272f3c9184b2d6b7c5b5bfba8bccd973
Renato <renatomarquesteles@gmail.com>
2020-04-02 11:27:21 -0300

    frontend: chore: configura store do redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
-import GlobalStyle from './styles/global';
-import Routes from './routes';
 import './config/ReactotronConfig';
+
+import Routes from './routes';
+import store from './store';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Routes />
       <GlobalStyle />
-    </>
+    </Provider>
   );
 }
 
