0822f6dad6667ce60dab3b87973dc87f1b8fcdc5
Renato <renatomarquesteles@gmail.com>
2020-04-03 11:49:39 -0300

    frontend: feat: estiliza listagem encomendas

--- frontend/src/pages/Deliveries/index.js
+++ frontend/src/pages/Deliveries/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiMoreHorizontal, FiPlus } from 'react-icons/fi';
 
-import { Content, Tools } from './styles';
 import { DefaultButton } from '~/components/Buttons/styles';
 import SearchInput from '~/components/Form/SearchInput';
 import api from '~/services/api';
+import { Table } from '~/components/Table/styles';
+import { Avatar, Content, Status, Tools } from './styles';
 
 export default function Deliveries() {
   api.get('/deliveries');
@@ -19,7 +20,7 @@ export default function Deliveries() {
           CADASTRAR
         </DefaultButton>
       </Tools>
-      <table>
+      <Table gridTemplateColumns={'0.5fr 2fr 2fr 1fr 1fr 1fr 0.5fr'}>
         <thead>
           <tr>
             <th>ID</th>
@@ -35,32 +36,71 @@ export default function Deliveries() {
           <tr>
             <td>#01</td>
             <td>Ludwig van Beethoven</td>
-            <td>John Doe</td>
+            <td>
+              <Avatar
+                src="https://api.adorable.io/avatars/40/deliveryman.png"
+                alt="Avatar"
+              />
+              John Doe
+            </td>
             <td>Rio do Sul</td>
             <td>Santa Catarina</td>
-            <td>Entregue</td>
-            <td>...</td>
+            <td>
+              <Status status="canceled">
+                <span />
+                Cancelado
+              </Status>
+            </td>
+            <td>
+              <FiMoreHorizontal size={18} color="#8c8c8c" />
+            </td>
           </tr>
           <tr>
             <td>#01</td>
             <td>Ludwig van Beethoven</td>
-            <td>John Doe</td>
+            <td>
+              <Avatar
+                src="https://api.adorable.io/avatars/40/deliveryman2.png"
+                alt="Avatar"
+              />
+              John Doe
+            </td>
             <td>Rio do Sul</td>
             <td>Santa Catarina</td>
-            <td>Entregue</td>
-            <td>...</td>
+            <td>
+              <Status status="delivered">
+                <span />
+                Entregue
+              </Status>
+            </td>
+            <td>
+              <FiMoreHorizontal size={18} color="#8c8c8c" />
+            </td>
           </tr>
           <tr>
             <td>#01</td>
             <td>Ludwig van Beethoven</td>
-            <td>John Doe</td>
+            <td>
+              <Avatar
+                src="https://api.adorable.io/avatars/40/deliveryman3.png"
+                alt="Avatar"
+              />
+              John Doe
+            </td>
             <td>Rio do Sul</td>
             <td>Santa Catarina</td>
-            <td>Entregue</td>
-            <td>...</td>
+            <td>
+              <Status status="pending">
+                <span />
+                Pendente
+              </Status>
+            </td>
+            <td>
+              <FiMoreHorizontal size={18} color="#8c8c8c" />
+            </td>
           </tr>
         </tbody>
-      </table>
+      </Table>
     </Content>
   );
 }
--- frontend/src/pages/Deliveries/styles.js
+++ frontend/src/pages/Deliveries/styles.js
@@ -1,4 +1,30 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const handleStatusBackground = (status) => {
+  switch (status) {
+    case 'delivered':
+      return '#DFF0DF';
+    case 'canceled':
+      return '#FAB0B0';
+    case 'pending':
+      return '#F0F0DF';
+    case 'withdrawal':
+      return '#BAD2FF';
+  }
+};
+
+const handleStatusColor = (status) => {
+  switch (status) {
+    case 'delivered':
+      return '#2CA42B';
+    case 'canceled':
+      return '#DE3B3B';
+    case 'pending':
+      return '#C1BC35';
+    case 'withdrawal':
+      return '#4D85EE';
+  }
+};
 
 export const Content = styled.div`
   width: 100%;
@@ -18,4 +44,32 @@ export const Tools = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+export const Avatar = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  margin-right: 5px;
+`;
+
+export const Status = styled.div`
+  background: ${(props) => handleStatusBackground(props.status)};
+  color: ${(props) => handleStatusColor(props.status)};
+  border-radius: 12px;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 7px;
+
+  display: flex;
+  align-items: center;
+
+  span {
+    height: 9px;
+    width: 9px;
+    border-radius: 50%;
+    background-color: ${(props) => handleStatusColor(props.status)};
+    margin-right: 5px;
+  }
 `;
