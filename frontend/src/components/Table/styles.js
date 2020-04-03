8b17c88a59cad7f43b38412dc1dccbd5b9314d1f
Renato <renatomarquesteles@gmail.com>
2020-04-03 11:49:21 -0300

    frontend: chore: cria componente table

---
+++ frontend/src/components/Table/styles.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  width: 100%;
+  display: block;
+
+  thead {
+    display: block;
+
+    tr {
+      display: grid;
+      grid-template-columns: ${(props) => props.gridTemplateColumns};
+
+      th {
+        min-height: 60px;
+        font-size: 16px;
+        margin-right: 5px;
+
+        display: flex;
+        align-items: center;
+
+        &:last-child {
+          justify-content: flex-end;
+        }
+      }
+    }
+  }
+
+  tbody {
+    display: block;
+
+    tr {
+      display: grid;
+      grid-template-columns: ${(props) => props.gridTemplateColumns};
+
+      background: #fff;
+      padding: 0 10px;
+
+      td {
+        min-height: 60px;
+        font-size: 16px;
+        margin-right: 5px;
+
+        display: flex;
+        align-items: center;
+
+        &:last-child {
+          justify-content: flex-end;
+        }
+      }
+
+      &:not(:first-child) {
+        margin: 15px 0;
+      }
+    }
+  }
+`;
